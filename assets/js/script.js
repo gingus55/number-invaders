1214b940d6ba04ce1b5f16a849a1f257d33ea207
gingus55 <gingus55@hotmail.com>
2022-10-14 16:02:09 +0100

    full 2D control

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,16 +6,65 @@ function createSquares(number) {
   for (let i = 0; i < number; i++) {
     const square = document.createElement("div");
     square.setAttribute("class", "square");
-    square.setAttribute("data", i);
+    square.setAttribute("data-number", i);
     gameGrid.appendChild(square);
   }
 }
 
 function placeRocket() {
-  const startLocation = document.querySelector('[data="95"]');
+  const startLocation = document.querySelector('[data-number="95"]');
   startLocation.setAttribute("class", "math-rocket");
+}
+
+function moveRocket(event) {
+  if (event.key == "ArrowUp") {
+    const current = document.querySelector(".math-rocket");
+    var location = parseInt(current.getAttribute("data-number"));
+    if (location > 10) {
+      location -= 10;
+      current.setAttribute("class", "square");
+      var string = "[data-number='" + location + "']";
+      const moved = document.querySelector(string);
+      moved.setAttribute("class", "math-rocket");
+    }
+  }
+  if (event.key == "ArrowLeft") {
+    const current = document.querySelector(".math-rocket");
+    var location = parseInt(current.getAttribute("data-number"));
+    if (!(location % 10 === 0)) {
+      location--;
+      current.setAttribute("class", "square");
+      var string = "[data-number='" + location + "']";
+      const moved = document.querySelector(string);
+      moved.setAttribute("class", "math-rocket");
+    }
+  }
+  if (event.key == "ArrowRight") {
+    const current = document.querySelector(".math-rocket");
+    var location = parseInt(current.getAttribute("data-number"));
+    if (!(location % 10 === 9)) {
+      location++;
+      current.setAttribute("class", "square");
+      var string = "[data-number='" + location + "']";
+      const moved = document.querySelector(string);
+      moved.setAttribute("class", "math-rocket");
+    }
+  }
+  if (event.key == "ArrowDown") {
+    const current = document.querySelector(".math-rocket");
+    var location = parseInt(current.getAttribute("data-number"));
+    if (location < 90) {
+      location += 10;
+      current.setAttribute("class", "square");
+      var string = "[data-number='" + location + "']";
+      const moved = document.querySelector(string);
+      moved.setAttribute("class", "math-rocket");
+    }
+  }
 }
 
 createSquares(100);
 
 placeRocket();
+
+addEventListener("keydown", moveRocket);
