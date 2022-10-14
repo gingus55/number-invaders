1cfe100a7b8a56b02aadefc8bbd9b1e42726caf7
gingus55 <gingus55@hotmail.com>
2022-10-14 14:51:38 +0100

    grid created rocket placed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,21 @@
 console.log("Howdy!");
+const gameGrid = document.querySelector("#game-grid");
+
+function createSquares(number) {
+  console.log(number + " squares are being created");
+  for (let i = 0; i < number; i++) {
+    const square = document.createElement("div");
+    square.setAttribute("class", "square");
+    square.setAttribute("data", i);
+    gameGrid.appendChild(square);
+  }
+}
+
+function placeRocket() {
+  const startLocation = document.querySelector('[data="95"]');
+  startLocation.setAttribute("class", "math-rocket");
+}
+
+createSquares(100);
+
+placeRocket();
